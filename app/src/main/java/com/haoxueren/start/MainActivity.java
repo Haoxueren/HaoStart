01d034096a8f0fa9a988c036f9c55fa0ff814bd4
Haoxueren <haoxueren@foxmail.com>
2017-08-17 16:06:47 +0800

    添加ReadMe.md文件

--- app/src/main/java/com/haoxueren/start/MainActivity.java
+++ app/src/main/java/com/haoxueren/start/MainActivity.java
@@ -2,13 +2,17 @@ package com.haoxueren.start;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethod;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -35,6 +39,8 @@ public class MainActivity extends BaseActivity {
     RecyclerView recyclerView;
     @BindView(R.id.refreshLayout)
     SwipeRefreshLayout refreshLayout;
+    @BindView(R.id.textInputLayout)
+    TextInputLayout textInputLayout;
     @BindView(R.id.appEditText)
     EditText appEditText;
 
@@ -42,6 +48,7 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_main);
+        appEditText.setHintTextColor(Color.WHITE);
         dao = super.application.getDaoSession().getHaoAppDao();
         recyclerView.setLayoutManager(new GridLayoutManager(this, 4));
         refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
@@ -53,21 +60,24 @@ public class MainActivity extends BaseActivity {
         appEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
                     // 查询符合条件的APP
                     String appName = v.getText().toString().trim();
                     if (appName.isEmpty()) {
-                        Toast.makeText(getApplicationContext(), "请输入应用名称", Toast.LENGTH_SHORT).show();
+                        textInputLayout.setError("请输入应用名称");
                         return true;
                     }
                     List<HaoApp> list = dao.queryBuilder()
                             .where(HaoAppDao.Properties.AppName.like(String.format("%%%s%%", appName)))
                             .orderDesc(HaoAppDao.Properties.AppName).list();
                     if (list.isEmpty()) {
-                        Toast.makeText(getApplicationContext(), "未查询到符合条件的应用", Toast.LENGTH_SHORT).show();
+                        textInputLayout.setError("未查询到符合条件的应用");
                     } else {
                         v.setText(null);
                         launchApp(list.get(0).getPackageName());
+                        // 隐藏软件键盘
+                        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                        inputMethodManager.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
                     }
                     return true;
                 }
@@ -77,6 +87,17 @@ public class MainActivity extends BaseActivity {
         loadAppFromSQLite();
     }
 
+
+    @Override
+    public void onBackPressed() {
+        this.moveTaskToBack(false);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        appEditText.clearFocus();
+    }
 
     public void onAddClick(View view) {
         Intent intent = new Intent(this, InstalledAppActivity.class);
@@ -111,6 +132,7 @@ public class MainActivity extends BaseActivity {
                     public void onItemLongClick(int position) {
                         dao.delete(haoApps.get(position));
                         loadAppFromSQLite();
+                        Toast.makeText(getApplicationContext(), "删除成功", Toast.LENGTH_SHORT).show();
                     }
                 }));
             }
